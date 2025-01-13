f1ec70958c799587e52913a910c661283ebd0f88
Dhanashree Trambadiya <dhanashreetrambadiya@gmail.com>
2025-01-13 22:42:50 +0530

    todo list initial commit

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+import os 
+  
+file_path = os.path.abspath(os.getcwd())+"/todo.db"
+  
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) 
+  
+  
+from app import routes 
